5b19b12747c0166e642f06b9ea6ca76511f9a9ea
pauloricardoprsm <pauloricardoprsm@gmail.com>
2022-10-11 11:31:18 -0300

    Usando Hooks para adicionar itens a uma lista

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
+let novoID = 4;
+
+export default function App(){
+  /* ### Hook
+  useState retorna um vetor com dois índices, sendo o primeiro o
+  valor do estado em si
+  */ 
+  const [repositories, setRepositories] = useState([
+    {id: 1, name: 'repo1'},
+    {id: 2, name: 'repo2'},
+    {id: 3, name: 'repo3'},
+    {id: 4, name: 'repo4'},
+  ]);
+
+
+  function handleAddRepository(){
+    novoID += 1;
+    setRepositories([
+      ...repositories, 
+      {id: novoID, name: "repo"+novoID}]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ul>
+        { repositories.map( repo => <li key={repo.id}>{repo.name}</li>)}
+      </ul>
+      <button onClick={handleAddRepository} >Adicionar Repositório</button>
+    </>
+  )
 }
 
-export default App;
